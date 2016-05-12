473cbb78481e4a00eee150c6175fb7e3089559b2
Rob Campbell <git@raacampbell.com>
2016-05-12 16:17:14 +0200

    Tidy

--- scanAndAcquire.m
+++ scanAndAcquire.m
@@ -14,6 +14,10 @@ function scanAndAcquire
 % Rob Campbell - Basel 2015
 
 
+	%Define a cleanup object that will release the DAQ gracefully
+	tidyUp = onCleanup(@stopAcq);
+
+
 	%----------------------------------
 	%User settings
 	amp=2; %Scanner amplitude
@@ -24,15 +28,14 @@ function scanAndAcquire
 	%----------------------------------
 
 
-	%Define a cleanup object in case the user does a ctrl-c and we end up with the 
-	%stages having peculiar speed settings, etc
-	tidyUp = onCleanup(@stopAcq);
+	%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	% CONNECT TO THE HARDWARE
 
 	%Create a session using NI hardware
 	s=daq.createSession('ni');
 
 
-	%Add an analog input channel
+	%Add an analog input channel for the PMT signal
 	AI=s.addAnalogInputChannel('Dev1', 'ai1', 'Voltage'); %Hard-coded. This is a PCI 6115
 	AI.Range = [-2,2];
 
@@ -40,44 +43,47 @@ function scanAndAcquire
 	%Add a listener to get data back from this channel
 	addlistener(s,'DataAvailable', @plotData); 
 
-	%Add analog output channels for scanners 0 is x and 1 is y
+
+	%Add analog two output channels for scanners 0 is x and 1 is y
 	s.addAnalogOutputChannel('Dev1',0:1,'Voltage'); %the 6115 is assigned to Dev1
 
+
+
+
+
+	%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	% BUILD THE GALVO WAVEFORMS
 
 	%Calculate the number of samples per line
 	samplesPerLine = pointsPerLine*samplesPerPoint;
 
+	%So the Y waveform is:
 	yWaveform = linspace(amp,-amp,samplesPerLine*linesPerFrame);
-
-
-	%The output buffer is re-filled for the next line when it becomes half empty
-	s.NotifyWhenScansQueuedBelow = round(length(yWaveform)*0.5); 
-
-
-	%The sample rate is fixed, so we report the frame rate
-	s.Rate = sampleRate;
-	frameRate = length(yWaveform)/sampleRate;
-	fprintf('Scanning at %0.2f frames per second\n',1/frameRate)
 
 	%Produce the X waveform
 	xWaveform = linspace(-amp, amp, samplesPerLine)*1;
 	xWaveform = repmat(xWaveform,1,length(yWaveform)/length(xWaveform));
-
 
 	%Assemble the two waveforms into an N-by-2 array
 	dataToPlay = [xWaveform(:),yWaveform(:)];
 	fprintf('Data waveforms have length %d\n',size(dataToPlay,1))
 	
 
-	%Plot the waveforms
-	hFig=clf;
-	plot(dataToPlay)
-	drawnow
-	
+
+
+	%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	% PREPARE TO ACQUIRE
+
+	%The sample rate is fixed, so we report the frame rate
+	s.Rate = sampleRate;
+	frameRate = length(yWaveform)/sampleRate;
+	fprintf('Scanning at %0.2f frames per second\n',1/frameRate)
+
+	%The output buffer is re-filled for the next line when it becomes half empty
+	s.NotifyWhenScansQueuedBelow = round(length(yWaveform)*0.5); 
 
 	%This listener tops up the output buffer
-	addlistener(s,'DataRequired', @(src,event) src.queueOutputData(dataToPlay)); %add listener on the DataRequired event to top it up
-
+	addlistener(s,'DataRequired', @(src,event) src.queueOutputData(dataToPlay));
 
 	s.IsContinuous = true; %needed to provide continuous behavior
 	s.queueOutputData(dataToPlay); %queue the first frame
@@ -86,23 +92,31 @@ function scanAndAcquire
 	s.NotifyWhenDataAvailableExceeds=size(dataToPlay,1); %when to read back
 
 
+
+
+	%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	% SET UP THE FIGURE WINDOW THAT WILL DISPLAY THE DATA
+
 	%We will plot the data on screen as they come in, so make a blank image
-	clf(hFig);
+	hFig=clf;
 	histAx=subplot(1,2,1);
 
 	imAx=subplot(1,2,2);
 	hAx=imagesc(zeros(linesPerFrame,pointsPerLine));
 	colormap gray
 
+
+
+
+	%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	% START!
 	s.startBackground %start the acquisition in the background
 
 	%Block. User presses ctrl-C to to quit, this calls stopAcq
 	while 1,pause(0.1), end
 
 
-
-
-
+	%-----------------------------------------------
 	function stopAcq
 		fprintf('Zeroing AO channels\n')
 		s.stop;
@@ -112,7 +126,7 @@ function scanAndAcquire
 
 		fprintf('Releasing NI hardware\n')
 		release(s);
-	end
+	end %stopAcq
 
 
 	function plotData(src,event)
@@ -134,7 +148,6 @@ function scanAndAcquire
 		set(hAx,'CData',im);
 		set(imAx,'CLim',[0,2]);
 		
-		
- 	end
+ 	end %plotData
 
-end
+end %scanAndAcquire
