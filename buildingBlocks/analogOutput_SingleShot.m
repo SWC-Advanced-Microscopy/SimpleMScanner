efcf738ab7c23e0eff19148a752a391f7054c9f0
Rob Campbell <git@raacampbell.com>
2016-05-13 19:09:51 +0200

    Begin to break down the process of building scanning software into a set of sub-functions.

---
+++ buildingBlocks/analogOutput_SingleShot.m
@@ -0,0 +1,30 @@
+function analogOutput_SingleShot
+% Play one cycle of a waveform out of an analog output
+%
+%
+%
+% Rob Campbell - Basel 2015
+
+
+%Create a session using NI hardware
+s=daq.createSession('ni');
+
+
+%Add one output channel (channel 0)
+s.addAnalogOutputChannel('Dev1',0,'Voltage'); 
+
+
+%Build one cycle of a sine wave
+waveForm=sin(-pi : pi/1000 : pi);
+
+
+%Set the sample rate to 2000 samples per second, so the waveform plays out in one second
+s.Rate = 2000;
+
+
+%Queue the data to the board
+s.queueOutputData(dataToPlay);
+
+
+%Play the waveform
+s.startForeground 
---
+++ buildingBlocks/cleanUp.m
@@ -0,0 +1,26 @@
+function cleanUp
+% Example - run a sub-function automatically when the caller function ends
+%
+% function cleanUp
+%
+% Purpose
+% Demo of how to run a sub-function when the caller function ends. 
+% The "cleanUp" function will run even if the caller crashes.
+%
+% Rob Campbell - Basel 2015
+
+
+%Define a cleanup object that will release the DAQ gracefully
+tidyUp = onCleanup(@cleanUpFunction);
+
+fprintf('Press ctrl-C to abort')
+	
+while 1
+	pause(0.5)
+	fprintf('.')
+end
+
+
+%-----------------------------------------------
+function cleanUpFunction
+	fprintf('\n\nTidying up.\n\n')
